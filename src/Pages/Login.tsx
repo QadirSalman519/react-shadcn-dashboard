6e5d667c630a7492997f1fd1bf35b8cc5837576d
QadirSalman519 <qadirsalman019@gmail.com>
2025-09-19 21:05:17 +0500

    Init

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,66 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+function Login() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 dar:bg-gray-900 w-full px-4">
+      <div className="flex flex-col items-center w-full max-w-md">
+        <Card className="w-full max-w-md text-center">
+          <CardHeader>
+            <div className="mb-6 text-center">
+              <img src="" alt="Logo" className="h-12 w-auto" />
+            </div>
+
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input id="password" type="password" required />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
